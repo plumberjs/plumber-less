d75f7373f9d6f6b371e070223af81c09a830b473
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-01-02 17:23:25 +0000

    Report all @imported files to supervisor

--- index.js
+++ index.js
@@ -4,17 +4,34 @@ var q = require('q');
 var less = require('less');
 var extend = require('extend');
 
-var render = q.denodeify(less.render);
+
+// Recursively extract "importedFilename" properties
+function collectFilenames(node) {
+    // FIXME: flaky way of doing this
+    var files = (node.rules || []).reduce(function(files, rule) {
+        if (rule.importedFilename) {
+            files = files.concat(rule.importedFilename);
+        }
+        return files.concat(collectFilenames(rule));
+    }, []);
+
+    return files;
+}
 
 
 module.exports = function(options) {
-    return mapEachResource(function(resource) {
+    return mapEachResource(function(resource, supervisor) {
         // TODO: map extra options (filename, paths, yuicompress, etc)?
         var parser = new less.Parser(extend({}, options, {
             filename: resource.filename()
         }));
         var parse = q.denodeify(parser.parse.bind(parser));
         return parse(resource.data()).then(function(tree) {
+            // Tell supervisor about all @imported files
+            collectFilenames(tree).forEach(function(dependencyFile) {
+                supervisor.dependOn(dependencyFile);
+            });
+
             var sourceMapData;
             var cssData = tree.toCSS({
                 sourceMap: true,
