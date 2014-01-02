df40568878ee8c763cbfe5f6126d674e3e5c8981
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-01-02 17:14:30 +0000

    Make LESS also return a sourcemap

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var mapEachResource = require('plumber').mapEachResource;
 
 var q = require('q');
 var less = require('less');
+var extend = require('extend');
 
 var render = q.denodeify(less.render);
 
@@ -9,8 +10,25 @@ var render = q.denodeify(less.render);
 module.exports = function(options) {
     return mapEachResource(function(resource) {
         // TODO: map extra options (filename, paths, yuicompress, etc)?
-        return render(resource.data(), options).then(function(cssData) {
-            return resource.replaceExtension('css').withData(cssData);
+        var parser = new less.Parser(extend({}, options, {
+            filename: resource.filename()
+        }));
+        var parse = q.denodeify(parser.parse.bind(parser));
+        return parse(resource.data()).then(function(tree) {
+            var sourceMapData;
+            var cssData = tree.toCSS({
+                sourceMap: true,
+                writeSourceMap: function writeSourceMap(data) {
+                    // this whole pseudo async is somewhat ridiculous
+                    sourceMapData = data;
+                }
+            });
+
+            var compiledCss = resource.replaceExtension('css').withData(cssData);
+            return [
+                compiledCss,
+                compiledCss.withExtension('map').withData(sourceMapData)
+            ];
         });
     });
 };
